77f8b3d44952110462d98ba066eea8d5f723bea9
chrislaniel <98767309+chrislaniel@users.noreply.github.com>
2022-02-28 17:53:53 -0500

    add __str__ to deck class

--- blackjack.py
+++ blackjack.py
@@ -4,13 +4,14 @@ suits = ("Hearts","Diamonds","Spades","Clubs")
 ranks = ("Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace")
 values = {"Two":2,"Three":3,"Four":4,"Five":5,"Six":6,"Seven":7,"Eight":8,"Nine":9,"Ten":10,"Jack":10,"Queen":10,"King":10,"Ace":11}
 
+playing = True
 
 class Card:
 
     def __init__(self,suit,rank):
         self.suit = suit
         self.rank = rank
-        self.values = values[rank]
+        self.value = values[rank]
 
     def __str__(self):
         return self.rank + " of " + self.suit
@@ -25,9 +26,12 @@ class Deck:
                 created_card = Card(suit,rank)
                 self.all_cards.append(created_card)
 
+    def __str__(self):
+        return "\n".join([str(card) for card in self.all_cards])
+
     def shuffle(self):
         random.shuffle(self.all_cards)
 
     def deal_one(self):
         return self.all_cards.pop()
-        
+    
